1621e03fa46f2e151bf6b1007ea33682ae75ab3e
Siarhei_Baradzionak <Siarhei_Baradzionak@tut.by>
2015-12-14 00:24:10 +0300

    added module 3

---
+++ Siarhei/JAVA_CDP_BY_03/module3.abstractFactory/src/main/java/com/epam/training/module3/factoryInterface/AbstractSourceFactory.java
@@ -0,0 +1,7 @@
+package com.epam.training.module3.factoryInterface;
+
+import com.epam.training.module3.sourceInterface.Source;
+
+public interface AbstractSourceFactory {
+    public Source createSource();
+}
---
+++ Siarhei/JAVA_CDP_BY_03/module3.abstractFactory/src/main/java/com/epam/training/module3/models/DBSourceFactory.java
@@ -0,0 +1,13 @@
+package com.epam.training.module3.models;
+
+import com.epam.training.module3.factoryInterface.AbstractSourceFactory;
+import com.epam.training.module3.sourceInterface.Source;
+
+public class DBSourceFactory implements AbstractSourceFactory {
+
+    public Source createSource() {
+        
+        return new DBSource();
+    }
+
+}
