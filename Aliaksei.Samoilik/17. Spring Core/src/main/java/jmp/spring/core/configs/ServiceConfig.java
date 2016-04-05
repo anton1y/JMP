bc9ea686a315d3748b9b316f90f43beb90442edf
Aliaksei Samoilik <Aliaksei_Samoilik@epam.com>
2016-04-05 23:22:30 +0300

    used @ComponentScan for services to show different injection scenarios

--- Aliaksei.Samoilik/17. Spring Core/src/main/java/jmp/spring/core/configs/ServiceConfig.java
+++ Aliaksei.Samoilik/17. Spring Core/src/main/java/jmp/spring/core/configs/ServiceConfig.java
@@ -1,21 +1,9 @@
 package jmp.spring.core.configs;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import jmp.spring.core.repositories.CustomerRepository;
-import jmp.spring.core.services.CustomerService;
-
 @Configuration
+@ComponentScan(basePackages = { "jmp.spring.core.services" })
 public class ServiceConfig {
-
-    @Autowired
-    CustomerRepository customerRepository;
-    @Bean
-    public CustomerService customerService() {
-        CustomerService customerService = new CustomerService();
-        customerService.setCustomerRepository(customerRepository);
-        return customerService;
-    }
 }
